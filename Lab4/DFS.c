adc4fceec856b94c2a649cb8a10ee6cee7434484
Sapthami Upadhya <157310822+sapthamiu@users.noreply.github.com>
2025-02-01 16:28:06 +0530

    Update DFS.c

--- Lab4/DFS.c
+++ Lab4/DFS.c
@@ -1,102 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define MAX_NODES 100  // Maximum number of vertices
+#define MAX 100
 
-typedef struct Node {
-    int vertex;
-    struct Node* next;
-} Node;
+int visited[MAX];
+int pushOrder[MAX];
+int popOrder[MAX];
+int pushInd = 0;
+int popInd = 0;
 
-typedef struct Graph {
-    int numVertices;
-    Node* adjLists[MAX_NODES];
-} Graph;
+void dfs(int adjMat[MAX][MAX], int n, int vertex){
+	visited[vertex] = 1;
+    pushOrder[pushInd++] = vertex;
 
-// Global variables
-int visited[MAX_NODES];    // Array to track visited vertices
-int pushOrder[MAX_NODES];  // Array to store push order
-int popOrder[MAX_NODES];   // Array to store pop order
-int pushIndex = 0;         // Index for push order
-int popIndex = 0;          // Index for pop order
-
-// Function prototypes
-Graph* createGraph(int vertices);
-void addEdge(Graph* graph, int src, int dest);
-void DFS(Graph* graph);
-void dfs(Graph* graph, int vertex);
-void printOrder(int order[], int size, const char* label);
-
-// Create a graph with given vertices
-Graph* createGraph(int vertices) {
-    Graph* graph = (Graph*)malloc(sizeof(Graph));
-    graph->numVertices = vertices;
-    for (int i = 0; i < vertices; i++) {
-        graph->adjLists[i] = NULL;
-        visited[i] = 0;  // Mark all vertices as unvisited
+    for (int i = 0; i < n; i++) {
+        if (adjMat[vertex][i]==1 && !visited[i])
+            dfs(adjMat, n, i);
     }
-    return graph;
+   popOrder[popInd++] = vertex;
 }
-
-// Add an edge to the graph
-void addEdge(Graph* graph, int src, int dest) {
-    Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->vertex = dest;
-    newNode->next = graph->adjLists[src];
-    graph->adjLists[src] = newNode;
-
-    // Since the graph is undirected, add the reverse edge
-    newNode = (Node*)malloc(sizeof(Node));
-    newNode->vertex = src;
-    newNode->next = graph->adjLists[dest];
-    graph->adjLists[dest] = newNode;
+void DFS(int adjMat[MAX][MAX], int n){
+	for(int v = 0; v < n; v++)
+		if(!visited[v])
+			dfs(adjMat, n, v);
 }
-
-// Perform DFS traversal for the entire graph
-void DFS(Graph* graph) {
-    for (int v = 0; v < graph->numVertices; v++) {
-        if (visited[v] == 0) {  // If vertex is unvisited
-            dfs(graph, v);
-        }
-    }
-}
-
-// Recursive DFS function for a single vertex
-void dfs(Graph* graph, int vertex) {
-    // Push order: Mark the vertex as visited and log it
-    visited[vertex] = 1;
-    pushOrder[pushIndex++] = vertex;
-
-    // Explore all adjacent vertices
-    Node* temp = graph->adjLists[vertex];
-    while (temp != NULL) {
-        int adjVertex = temp->vertex;
-        if (visited[adjVertex] == 0) {  // If adjacent vertex is unvisited
-            dfs(graph, adjVertex);
-        }
-        temp = temp->next;
-    }
-
-    // Pop order: Log the vertex when exploration is complete
-    popOrder[popIndex++] = vertex;
-}
-
-// Print the push/pop order
-void printOrder(int order[], int size, const char* label) {
+void printOrder(int order[], int n, const char* label) {
     printf("%s Order:\n", label);
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%d ", order[i]);
     }
     printf("\n");
 }
 
-// Main function
 int main() {
     int vertices, edges;
     printf("Enter the number of vertices: ");
     scanf("%d", &vertices);
 
-    Graph* graph = createGraph(vertices);
+    int adjMat[MAX][MAX] = {0};  // Initialize adjacency matrix to 0
 
     printf("Enter the number of edges: ");
     scanf("%d", &edges);
@@ -105,15 +46,16 @@ int main() {
     for (int i = 0; i < edges; i++) {
         int src, dest;
         scanf("%d %d", &src, &dest);
-        addEdge(graph, src, dest);
+        adjMat[src][dest] = 1;  // Set edge from src to dest
+        adjMat[dest][src] = 1;  // Since the graph is undirected, set the reverse edge too
     }
 
     printf("Performing Depth-First Search (DFS)...\n");
-    DFS(graph);
+    DFS(adjMat, vertices);
     
     // Print push and pop order
-    printOrder(pushOrder, pushIndex, "Push");
-    printOrder(popOrder, popIndex, "Pop");
+    printOrder(pushOrder, pushInd, "Push");
+    printOrder(popOrder, popInd, "Pop");
 
     return 0;
 }
