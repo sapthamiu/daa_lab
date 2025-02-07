da5a065f42efb49278cead0b8b2eff001a886b4f
sapthamiu <sapthami.mitmpl2023@learner.manipal.edu>
2025-02-07 10:23:08 +0530

    late update

--- Lab2/CICGCD.c
+++ Lab2/CICGCD.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
+
+int fibonacci(int n){
+	if(n == 0)
+		return 0;
+	if(n == 1)
+		return 1;
+	return fibonacci(n-1) + fibonacci(n-2);
+}
 int CIC(int n1, int n2, int* opcount){
 	
 	int t = n1 < n2 ? n1 : n2;
@@ -10,9 +19,16 @@ int CIC(int n1, int n2, int* opcount){
 	return t;
 }
 void main(){
-	int a, b, count = 0;
+	int a, b, count = 1;
 	printf("Enter two numbers: ");
-	scanf("%d%d", &a,&b);
-	printf("GCD = %d", CIC(a,b, &count));
-	printf("\nOperation count: %d", count);
+	srand(time(NULL));
+	int n = rand() % 10 + 1;
+	a = fibonacci(n);
+	b = fibonacci(n+1);
+	
+	// scanf("%d %d", &a, &b);
+	printf("GCD  of %d and %d = %d", a, b, CIC(a,b, &count));
+
+	printf("\n(m+n): %d Operation count: %d", a+b, count);
 }
+
